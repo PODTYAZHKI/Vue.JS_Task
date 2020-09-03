62c4b56d0b5300a45cba6be4a308a130585b64a2
Sergey Muzychuk <supper.ser2001@gmail.com>
2020-09-03 23:48:38 +0300

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,206 @@
+<template>
+	<form action="/sonething" id="app" @submit="ActionPost">
+			<div class="login__item">
+				<div class="login__text">Логин</div>
+				<input
+					type="text" 
+					name="login" 
+					id="login" 
+					v-model.trim="login"
+					:class="{log__error:error.login}"
+				>
+			</div>
+			<p v-if="error.login">{{error.login}}</p>
+			<div class="login__item">
+				<div class="login__text">Телефон</div>
+				<input 
+				type="text" 
+				name="phone" 
+				id="phone"
+				v-model.trim="phone"
+				:class="{log__error:error.phone}"
+				>
+			</div>
+			<p v-if="error.phone">{{error.phone}}</p>
+			<div class="login__item">
+				<div class="login__text">Пароль</div>
+				<input 
+				type="password" 
+				name="password" 
+				id="password"
+				v-model.trim="password"
+				:class="{log__error:error.password}"
+				>
+			</div>
+			<p v-if="error.password">{{error.password}}</p>
+			<input type="submit" value="Войти">
+		</form>
+</template>
+
+<script>
+
+const loginCheck = /^[a-z0-9_]{5,}$/;
+const phoneCheck = /^[0-9]{11}$/;
+
+export default{
+
+	data(){
+		return{
+			login: '',
+			phone: '',
+			password: '',
+			error: [],
+			isPressed: false
+		};
+	},
+
+	computed: {
+		isLoginValid() {
+			return loginCheck.test(this.login);
+		},
+		isPhoneValid() {
+			return phoneCheck.test(this.phone);
+		},
+		isPasswordValid() {
+			return (this.password.length >= 6)?true:false;
+		},
+
+
+		isPasswordEmpty() {
+			return (!this.password)?true:false;
+		}
+		// LoginError() {
+		// 	return this.error['login'] = 'Неверный адрес электронной почты';
+		// }
+	},
+
+	watch:{
+		login(value){
+			this.login = value;
+			this.validateLogin(value);
+		},
+		phone(value){
+			this.phone = value;
+			this.validatePhone(value);
+		},
+		password(value){
+			this.password = value;
+			this.validatePassword(value);
+		}
+	},
+	methods: {
+
+		validateLogin(value){
+			if (this.isPressed){
+				if (loginCheck.test(this.login)) this.error['login'] = ''
+				else if (!this.login) this.error['login'] = 'Обязательное поле'
+				else this.error['login'] = 'Неверный адрес электронной почты';
+			}
+		},
+		validatePhone(value){
+			if (this.isPressed){
+				if (phoneCheck.test(this.phone)) this.error['phone'] = ''
+				else if (!this.phone) this.error['phone'] = 'Обязательное поле'
+				else this.error['phone'] = 'Неверный телефон';
+			}
+		},
+		validatePassword(value){
+			if (this.isPressed){
+				if (this.password.length >= 6) this.error['password'] = ''
+				else if (!this.password) this.error['password'] = 'Обязательное поле'
+				else this.error['password'] = 'Неверный пароль';
+			}
+		},
+		// validatePassword(value){
+		// 	if (this.isPressed){
+		// 		if (this.password.value === null || this.password.value === '') this.error['password'] = 'Неверный пароль'
+		// 		else this.error['password'] = '';
+		// 	}
+		// },
+
+
+		ActionPost: function(e) {
+			this.isPressed = true;
+			if(!this.isLoginValid || !this.isPhoneValid || !this.isPasswordValid) {
+				e.preventDefault();
+				if (!this.isLoginValid) this.error['login'] = 'Неверный адрес электронной почты';
+				if (!this.isPhoneValid) this.error['phone'] = 'Неверный телефон';
+				if (!this.isPasswordValid) {this.error['password'] = 'Неверный пароль'; this.$forceUpdate()}
+				if (!this.login){this.error['login'] = 'Обязательное поле'}
+				if (!this.phone){this.error['phone'] = 'Обязательное поле'}
+				if (!this.password){this.error['password'] = 'Обязательное поле'; this.$forceUpdate()}
+				console.log(this.error);
+				}
+				
+			else alert("Форма отправлена!");
+			
+
+
+		}
+	}
+}
+</script>>
+
+<style>
+
+form{
+	margin: 0 auto;
+	width: 200px;
+}
+
+.login__text{
+	font-size: 16px;
+	font-weight: bold;
+	padding-bottom: 10px;
+}
+
+input[type=text], input[type=password] {
+	width: 100%;
+	height: 30px;
+	padding-left: 10px;
+	margin-bottom: 10px;
+	text-decoration: none;
+	outline: none;
+	border: 1px solid rgb(204, 204, 204);
+	border-radius: 3px;
+}
+
+input[type=text]:focus{
+	border-color: rgb(54, 139, 250);
+	outline: none;
+	/* text-decoration: none; */
+}
+
+input[type=submit]{
+	width: 60px;
+	height: 30px;
+	cursor: pointer;
+	background-color: #fff;
+	text-decoration: none;
+	border: 1px solid rgb(204, 204, 204);
+	border-radius: 3px;
+	color: rgb(139, 139, 139);
+	transition: background linear .3s;
+}
+
+input[type=submit]:hover{
+	background-color: rgb(54, 139, 250);
+	color: #fff;
+	border: none;
+}
+
+input[type=submit]:focus {
+	text-decoration: none;
+	border-color: rgb(54, 139, 250);
+}
+
+p {
+	padding-top: 0px;
+	margin-top: 0px;
+	font-size: 12px; color:red;
+}
+
+.log__error {
+	border: 1px solid red !important;
+}
+</style>>
